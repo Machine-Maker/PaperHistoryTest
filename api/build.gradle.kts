17c96b4df65e25c2b645d9be990ade0fcce90c10
Zach Brown <zach.brown@destroystokyo.com>
2016-03-01 00:16:08 +0100

    Build system changes

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -14,14 +14,26 @@ dependencies {
     api("com.google.code.gson:gson:2.8.9")
     api("net.md-5:bungeecord-chat:1.16-R0.4")
     api("org.yaml:snakeyaml:1.30")
+    // Paper start
+    api("com.googlecode.json-simple:json-simple:1.1.1") {
+        isTransitive = false // includes junit
+    }
+    // Paper end
 
     compileOnly("org.apache.maven:maven-resolver-provider:3.8.5")
     compileOnly("org.apache.maven.resolver:maven-resolver-connector-basic:1.7.3")
     compileOnly("org.apache.maven.resolver:maven-resolver-transport-http:1.7.3")
+    compileOnly("com.google.code.findbugs:jsr305:1.3.9") // Paper
 
-    val annotations = "org.jetbrains:annotations-java5:23.0.0"
+    val annotations = "org.jetbrains:annotations:23.0.0" // Paper - we don't want Java 5 annotations...
     compileOnly(annotations)
     testCompileOnly(annotations)
+
+    // Paper start - add checker
+    val checkerQual = "org.checkerframework:checker-qual:3.21.0"
+    compileOnlyApi(checkerQual)
+    testCompileOnly(checkerQual)
+    // Paper end
 
     testImplementation("org.apache.commons:commons-lang3:3.12.0")
     testImplementation("junit:junit:4.13.2")
@@ -64,7 +76,7 @@ tasks.withType<Javadoc> {
     options.links(
         "https://guava.dev/releases/31.0.1-jre/api/docs/",
         "https://javadoc.io/doc/org.yaml/snakeyaml/1.30/",
-        "https://javadoc.io/doc/org.jetbrains/annotations-java5/23.0.0/",
+        "https://javadoc.io/doc/org.jetbrains/annotations/23.0.0/", // Paper - we don't want Java 5 annotations
         "https://javadoc.io/doc/net.md-5/bungeecord-chat/1.16-R0.4/",
     )
 
@@ -79,3 +91,14 @@ tasks.withType<Javadoc> {
         }
     }
 }
+
+// Paper start
+val scanJar = tasks.register("scanJarForBadCalls", io.papermc.paperweight.tasks.ScanJarForBadCalls::class) {
+    badAnnotations.add("Lio/papermc/paper/annotation/DoNotUse;")
+    jarToScan.set(tasks.jar.flatMap { it.archiveFile })
+    classpath.from(configurations.compileClasspath)
+}
+tasks.check {
+    dependsOn(scanJar)
+}
+// Paper end
