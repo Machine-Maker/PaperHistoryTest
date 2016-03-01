76933fbade4efd6c2af697708a91240cb706877f
Steve Anton <anxuiz.nx@gmail.com>
2016-02-29 18:13:58 -0600

    Add PlayerInitialSpawnEvent
    For modifying a player's initial spawn location as they join the server

---
+++ api/src/main/java/com/destroystokyo/paper/event/player/PlayerInitialSpawnEvent.java
@@ -0,0 +1,17 @@
+package com.destroystokyo.paper.event.player;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.spigotmc.event.player.PlayerSpawnLocationEvent;
+
+/**
+ * @deprecated Use {@link PlayerSpawnLocationEvent}, Duplicate API
+ */
+@Deprecated
+public class PlayerInitialSpawnEvent extends PlayerSpawnLocationEvent {
+
+    public PlayerInitialSpawnEvent(@NotNull Player who, @NotNull Location spawnLocation) {
+        super(who, spawnLocation);
+    }
+}
