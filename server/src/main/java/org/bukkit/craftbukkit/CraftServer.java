65d18584cc4d06c35a4586fcda3a55e061762d47
Sweepyoface <github@sweepy.pw>
2017-06-17 18:48:21 -0400

    Add UnknownCommandEvent

--- server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -890,7 +890,13 @@ public final class CraftServer implements Server {
 
         // Spigot start
         if (!org.spigotmc.SpigotConfig.unknownCommandMessage.isEmpty()) {
-            sender.sendMessage(org.spigotmc.SpigotConfig.unknownCommandMessage);
+            // Paper start
+            org.bukkit.event.command.UnknownCommandEvent event = new org.bukkit.event.command.UnknownCommandEvent(sender, commandLine, org.spigotmc.SpigotConfig.unknownCommandMessage);
+            Bukkit.getServer().getPluginManager().callEvent(event);
+            if (event.message() != null) {
+                sender.sendMessage(event.message());
+            }
+            // Paper end
         }
         // Spigot end
 
