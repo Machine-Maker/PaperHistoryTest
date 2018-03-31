95cda4ea40cdfe6f32949dccbeb1efe09b3b2bd7
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-04-01 02:28:43 +0300

    Add openSign method to HumanEntity

--- api/src/main/java/org/bukkit/entity/Player.java
+++ api/src/main/java/org/bukkit/entity/Player.java
@@ -2153,7 +2153,7 @@ public interface Player extends HumanEntity, Conversable, OfflinePlayer, PluginM
     /**
      * Open a Sign for editing by the Player.
      *
-     * The Sign must be placed in the same world as the player.
+     * The Sign must be in the same world as the player.
      *
      * @param sign The sign to edit
      */
