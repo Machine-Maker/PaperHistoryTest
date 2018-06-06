acdeb326b66a98a0171f6b2fc879afb3d74636be
Aikar <aikar@aikar.co>
2018-06-05 22:59:50 -0400

    ItemStack#getMaxItemUseDuration
    Allows you to determine how long it takes to use a usable/consumable item

--- api/src/main/java/org/bukkit/inventory/ItemStack.java
+++ api/src/main/java/org/bukkit/inventory/ItemStack.java
@@ -639,5 +639,13 @@ public class ItemStack implements Cloneable, ConfigurationSerializable, net.kyor
     public String getI18NDisplayName() {
         return Bukkit.getServer().getItemFactory().getI18NDisplayName(this);
     }
+
+    public int getMaxItemUseDuration() {
+        if (type == null || type == Material.AIR || !type.isItem()) {
+            return 0;
+        }
+        // Requires access to NMS
+        return ensureServerConversions().getMaxItemUseDuration();
+    }
     // Paper end
 }
