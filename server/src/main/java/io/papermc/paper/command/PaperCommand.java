e8afd728390dfa8285d2fb977fed2aa6ad8bc738
Spottedleaf <Spottedleaf@users.noreply.github.com>
2019-07-19 03:29:14 -0700

    Add debug for sync chunk loads
    This patch adds a tool to find calls to getChunkAt which would load
    chunks, however it must be enabled by setting the startup flag
    -Dpaper.debug-sync-loads=true
    
    - To get a debug log for sync loads, the command is
      /paper syncloadinfo
    - To clear clear the currently stored sync load info, use
      /paper syncloadinfo clear

--- server/src/main/java/io/papermc/paper/command/PaperCommand.java
+++ server/src/main/java/io/papermc/paper/command/PaperCommand.java
@@ -5,6 +5,7 @@ import io.papermc.paper.command.subcommands.EntityCommand;
 import io.papermc.paper.command.subcommands.FixLightCommand;
 import io.papermc.paper.command.subcommands.HeapDumpCommand;
 import io.papermc.paper.command.subcommands.ReloadCommand;
+import io.papermc.paper.command.subcommands.SyncLoadInfoCommand;
 import io.papermc.paper.command.subcommands.VersionCommand;
 import it.unimi.dsi.fastutil.Pair;
 import java.util.ArrayList;
@@ -44,6 +45,7 @@ public final class PaperCommand extends Command {
         commands.put(Set.of("version"), new VersionCommand());
         commands.put(Set.of("debug", "chunkinfo"), new ChunkDebugCommand());
         commands.put(Set.of("fixlight"), new FixLightCommand());
+        commands.put(Set.of("syncloadinfo"), new SyncLoadInfoCommand());
 
         return commands.entrySet().stream()
             .flatMap(entry -> entry.getKey().stream().map(s -> Map.entry(s, entry.getValue())))
