f17067ab922356cc39f45649188ee904fb9b6cb4
Spottedleaf <spottedleaf@spottedleaf.dev>
2020-06-03 11:37:13 -0700

    Optimise getType calls
    Remove the map lookup for converting from Block->Bukkit Material

--- server/src/main/java/org/bukkit/craftbukkit/block/data/CraftBlockData.java
+++ server/src/main/java/org/bukkit/craftbukkit/block/data/CraftBlockData.java
@@ -50,7 +50,7 @@ public class CraftBlockData implements BlockData {
 
     @Override
     public Material getMaterial() {
-        return CraftMagicNumbers.getMaterial(this.state.getBlock());
+        return this.state.getBukkitMaterial(); // Paper - optimise getType calls
     }
 
     public BlockState getState() {
