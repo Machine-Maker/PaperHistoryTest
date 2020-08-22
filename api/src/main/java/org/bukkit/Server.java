c0e341105020b3c448922b31150792615b3b5393
Mariell Hoversholm <proximyst@proximyst.com>
2020-08-22 23:59:25 +0200

    Add #setMaxPlayers API

--- api/src/main/java/org/bukkit/Server.java
+++ api/src/main/java/org/bukkit/Server.java
@@ -159,6 +159,15 @@ public interface Server extends PluginMessageRecipient, net.kyori.adventure.audi
      */
     public int getMaxPlayers();
 
+    // Paper start
+    /**
+     * Set the maximum amount of players which can login to this server.
+     *
+     * @param maxPlayers the amount of players this server allows
+     */
+    public void setMaxPlayers(int maxPlayers);
+    // Paper end
+
     /**
      * Get the game port that the server runs on.
      *
