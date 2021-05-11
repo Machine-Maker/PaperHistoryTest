93bb779601916befa269f515ee2fa71b6281deb6
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-05-11 14:54:20 -0700

    Inventory#close

--- api/src/main/java/org/bukkit/inventory/Inventory.java
+++ api/src/main/java/org/bukkit/inventory/Inventory.java
@@ -352,6 +352,15 @@ public interface Inventory extends Iterable<ItemStack> {
      */
     public void clear();
 
+    // Paper start
+    /**
+     * Closes the inventory for all viewers.
+     *
+     * @return the number if viewers the inventory was closed for
+     */
+    public int close();
+    // Paper end
+
     /**
      * Gets a list of players viewing the inventory. Note that a player is
      * considered to be viewing their own inventory and internal crafting
