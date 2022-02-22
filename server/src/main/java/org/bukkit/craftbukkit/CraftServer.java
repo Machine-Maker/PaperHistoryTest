8f7363440b0c2e2de04916a179f280e9647bdfc0
Spottedleaf <Spottedleaf@users.noreply.github.com>
2022-02-22 14:21:35 -0800

    Put world into worldlist before initing the world
    Some parts of legacy conversion will need the overworld
    to get the legacy structure data storage

--- server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -1239,10 +1239,11 @@ public final class CraftServer implements Server {
             return null;
         }
 
+        console.levels.put(internal.dimension(), internal); // Paper - move up
         this.console.initWorld(internal, worlddata, worlddata, worlddata.worldGenSettings());
 
         internal.setSpawnSettings(true, true);
-        console.levels.put(internal.dimension(), internal);
+        // Paper - move up
 
         this.getServer().prepareLevels(internal.getChunkSource().chunkMap.progressListener, internal);
         internal.entityManager.tick(); // SPIGOT-6526: Load pending entities so they are available to the API
